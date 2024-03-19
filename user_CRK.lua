98bc894fb591db44b0bbf854076ef49d603be17f
EnJirad <130825937+EnJirad@users.noreply.github.com>
2024-03-19 22:40:15 +0700

    Update and rename user_CRK.txt to user_CRK.lua

---
+++ user_CRK.lua
@@ -0,0 +1,14 @@
+local function Users_CRK()
+return [[
+{
+    No = "1",
+    ID = "JCJIRU",
+    Key = "3cHnw0lB2iZRTHF2UBHtpCLYPlL5SGgC"
+},
+{
+    No = "2",
+    ID = "0MJA2Z",
+    Key = "DjzqWK95sZQplo4m4GvFkkcXVOk3biEW"
+}
+]]
+end

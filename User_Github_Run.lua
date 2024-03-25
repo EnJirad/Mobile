3f29d6e7e01c571fa302e7ed5f37ad532625747a
EnJirad <130825937+EnJirad@users.noreply.github.com>
2024-03-25 11:50:55 +0700

    Add files via upload

--- User_Github_Run.lua
+++ User_Github_Run.lua
@@ -70,13 +70,14 @@ end
 -- ตรวจสอบ HWID และ Key
 local validateHwidAndKey = function(content)
     if content then
-        local encrypted_ID, encrypted_Key, encrypted_start_time, encrypted_end_time = content:match("([^|]+)|([^|]+)|([^|]+)|([^|]+)")
-        if encrypted_ID and encrypted_Key and encrypted_start_time and encrypted_end_time then
+        local encrypted_ID, encrypted_Key, encrypted_Status, encrypted_start_time, encrypted_end_time = content:match("([^|]+)|([^|]+)|([^|]+)|([^|]+)|([^|]+)")
+        if encrypted_ID and encrypted_Key and encrypted_Status and encrypted_start_time and encrypted_end_time then
             local decrypted_ID = decryptNumberAndSymbol(encrypted_ID, shift_amount)
             local decrypted_Key = decryptNumberAndSymbol(encrypted_Key, shift_amount)
+            local decrypted_Status = decryptNumberAndSymbol(encrypted_Status, shift_amount)
             local decrypted_start_time = decryptDateTime(encrypted_start_time, shift_amount)
             local decrypted_end_time = decryptDateTime(encrypted_end_time, shift_amount)
-            return { ID = decrypted_ID, Key = decrypted_Key, Start = decrypted_start_time, End = decrypted_end_time }
+            return { ID = decrypted_ID, Key = decrypted_Key, Status = decrypted_Status, Start = decrypted_start_time, End = decrypted_end_time }
         end
     end
     return nil
@@ -110,18 +111,66 @@ local success, decryptedData = checkFileForKey()
 local Tin = gg.alert('เลือก วิธีที่คุณต้องการ','เข้าสู่ระบบ','เพิ่มเวลา')
 
 if Tin == 1 then
-    if success then
-        local API_URL = "https://raw.githubusercontent.com/EnJirad/Mobile/main/Cookie_RK_B.lua"
-        local response = gg.makeRequest(API_URL)
-        if response and response.content then
-            local success, userContent = pcall(load(response.content))
+    local GG = gg.PACKAGE
+    local file_path = "/storage/emulated/0/Android/data/" .. GG .. "/cache/"
+    local Check_User = file_path .. "register_user.lua"
+    
+    -- โหลดข้อมูลผู้ใช้จาก GitHub
+    local API_URL = "https://raw.githubusercontent.com/EnJirad/Mobile/main/User_CRK.lua"
+    local API = gg.makeRequest(API_URL).content
+    if API then
+        local success, userContent = pcall(load(API))
+        if not success then
+            print("ข้อผิดพลาด: ไม่สามารถเข้าถึง สคริปต์ ได้")
+            return
         end
     else
-        local API_URL = "https://raw.githubusercontent.com/EnJirad/Mobile/main/Main_Github_V3.lua"
-        local response = gg.makeRequest(API_URL)
-        if response and response.content then
-            local success, userContent = pcall(load(response.content))
+        print("ข้อผิดพลาด: ไม่สามารถเข้าถึง สคริปต์ ได้")
+        return
+    end
+    
+    local crkFile = io.open(Key_File, "r")
+    if crkFile then
+        local crkContent = crkFile:read("*all")
+        crkFile:close()
+        local crkData = validateHwidAndKey(crkContent)
+        local userFile = io.open(Check_User, "r")
+        if userFile then
+            local userContent = userFile:read("*all")
+            userFile:close()
+
+            local pattern = '{%s*No%s*=%s*"(%d+)"%s*,%s*ID%s*=%s*"(%w+)"%s*,%s*Key%s*=%s*"(%w+)"%s*,%s*Status%s*=%s*"(%a+)"%s*,%s*Time%s*=%s*"(%d+)"%s*}'
+            local users = {}
+            for No, ID, Key, Status, Time in userContent:gmatch(pattern) do
+                table.insert(users, {No = No, ID = ID, Key = Key, Status = Status, Time = Time})
+            end
+            
+            for _, user in ipairs(users) do
+                if user.ID == crkData.ID and user.Key == crkData.Key then
+                    if user.Status == crkData.Status then
+                        gg.alert("กรุณาติดต่อ แอดมิน เพื่อ เซ็ต ระบบให้ก่อน\n\nสถานะ : false")
+                    else
+                        if success then
+                            local API_URL = "https://raw.githubusercontent.com/EnJirad/Mobile/main/Cookie_RK_B.lua"
+                            local response = gg.makeRequest(API_URL)
+                            if response and response.content then
+                                local success, userContent = pcall(load(response.content))
+                            end
+                        else
+                            local API_URL = "https://raw.githubusercontent.com/EnJirad/Mobile/main/Main_Github_V3.lua"
+                            local response = gg.makeRequest(API_URL)
+                            if response and response.content then
+                                local success, userContent = pcall(load(response.content))
+                            end
+                        end
+                    end
+                end
+            end
+        else
+            print("เกิดข้อผิดพลาดในการเปิดไฟล์ register_user.lua")
         end
+    else
+        print("เกิดข้อผิดพลาดในการเปิดไฟล์ .com.google.android.crk1")
     end
     
 elseif Tin == 2 then
@@ -171,8 +220,8 @@ elseif Tin == 2 then
     local secondsPerDay = 24 * 60 * 60
     local remainingSeconds = endDate - os.time()
     remainingDays = math.floor(remainingSeconds / secondsPerDay)
-    local decryptedDataString = "ID: " .. decryptedData.ID .. "\nKey: " .. decryptedData.Key .. "\nเริ่มใช้งาน: " .. decryptedData.Start .. " น.\nหมดอายุ: " .. decryptedData.End .. " น."
-    gg.alert(decryptedDataString .. "\nเวลาเหลืออีก: " .. remainingDays .. " วัน")
+    local decryptedDataString = "ID : " .. decryptedData.ID .. "\nKey : " .. decryptedData.Key .. "\nสถานะ : " .. decryptedData.Status .. "\nเริ่มใช้งาน : " .. decryptedData.Start .. " น.\nหมดอายุ : " .. decryptedData.End .. " น."
+    gg.alert(decryptedDataString .. "\nเวลาเหลืออีก : " .. remainingDays .. " วัน")
     
     if success then
         local GG = gg.PACKAGE
